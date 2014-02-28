9b2a4fe30ca7ea55b502cd1995f864ad48176dfa
U-Nuggz-TNK\Deneb <Deneb@Nuggz-TNK.(none)>
2014-02-27 23:44:50 -0600

    Created manifest

---
+++ manifest.js
@@ -0,0 +1,12 @@
+{
+  "name": "SXSW Do512 RSVP", 
+  "description": "My first Chrome App.", 
+  "version": "0.1", 
+  "app": {
+    "background": {
+      "scripts": ["background.js"]
+    }
+  }, 
+  "icons": { "16": "icon.png" },
+}
+
